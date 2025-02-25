636c61ed6c480b46f9d1dee5905d293f0c99cb4c
Emanuel Ernesto de Andrade Rego <emanuel.rego@compasso.com.br>
2025-02-25 12:34:54 -0300

    fix(products): add constructors for dto classes

--- src/main/java/com/emanueldev/sample_shop/domain/dtos/response/PaginatedProductResponseDTO.java
+++ src/main/java/com/emanueldev/sample_shop/domain/dtos/response/PaginatedProductResponseDTO.java
@@ -1,9 +1,6 @@
 package com.emanueldev.sample_shop.domain.dtos.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.List;
 
@@ -11,6 +8,8 @@ import java.util.List;
 @Setter
 @ToString
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaginatedProductResponseDTO {
 
     List<ProductResponseDTO> products;
--- src/main/java/com/emanueldev/sample_shop/domain/dtos/response/ProductResponseDTO.java
+++ src/main/java/com/emanueldev/sample_shop/domain/dtos/response/ProductResponseDTO.java
@@ -1,14 +1,14 @@
 package com.emanueldev.sample_shop.domain.dtos.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponseDTO {
 
     private UUID productId;
