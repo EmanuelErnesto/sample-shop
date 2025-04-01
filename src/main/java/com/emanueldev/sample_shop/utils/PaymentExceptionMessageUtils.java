73cd846bec4fbc4a9a082d4e568191b050154836
EmanuelErnesto <emanuel.earego@gmail.com>
2025-04-01 02:01:39 -0300

    raw: add exception message utils of entities

---
+++ src/main/java/com/emanueldev/sample_shop/utils/PaymentExceptionMessageUtils.java
@@ -0,0 +1,8 @@
+package com.emanueldev.sample_shop.utils;
+
+public class PaymentExceptionMessageUtils {
+    public static final String PAYMENT_NOT_FOUND = "Payment not found";
+    public static final String PAYMENT_ALREADY_EXISTS = "This order already have other payment request.";
+    public static final String PAYMENT_NOT_AUTHORIZED = "The payment is not authorized by provider.";
+    public static final String PAYMENT_ALREADY_CREATED = "Payment for this order has already been made and validated.";
+}
